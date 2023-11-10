2f8697e385a7f3f916cee66c1758b5424d6ae4dd
prshntshinde <mr.pshinde@gmail.com>
2023-11-10 19:11:57 +0530

    feat: add navbar hover

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -3,12 +3,20 @@ import Link from 'next/link'
 import React from 'react'
 
 const NavBar = () => {
+    const links = [
+        { label: 'Dashboard', href: '/'},
+        {label: 'Quiz', href: '/quiz'}
+    ]
+
   return (
-    <nav>
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href="/">Logo</Link>
-        <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/quiz">Quiz</Link></li>
+        <ul className='flex space-x-6'>
+            {links.map(link => 
+                <Link 
+                    key={link.href}
+                    className='text-zinc-500 hover:text-zinc-900 transition-colors' 
+                    href={link.href}>{link.label}</Link>)}
         </ul>
     </nav>
   )
