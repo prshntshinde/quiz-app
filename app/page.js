a3ef04cdff28eaf18162a38aa91d6ba2a2d6ee05
prshntshinde <mr.pshinde@gmail.com>
2023-11-10 16:50:04 +0530

    fate: add navbar component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import NavBar from './components/NavBar'
 
 export default function Home() {
   return (
     <main>
       <p>Home Page</p>
       <Link href="/quiz">Quiz</Link>
+      <NavBar/>
     </main>
   )
 }
