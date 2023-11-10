3ca885355e03d1fdedfee7b0147b990b4ef1409c
prshntshinde <mr.pshinde@gmail.com>
2023-11-10 15:03:48 +0530

    fix: clean the css

--- app/page.js
+++ app/page.js
@@ -3,13 +3,11 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <p>Home Page</p>
       <Link href="/quiz">Quiz</Link>
     </main>
   )
 }
 
-export const metadata = {
-  title: 'Home |',
-}
+
--- app/quiz/page.js
+++ app/quiz/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Quiz() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <p>Quiz Page</p>
     </main>
   )
