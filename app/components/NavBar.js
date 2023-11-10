c6b1df49015521a482319ea9b95b3bff09bfe40a
prshntshinde <mr.pshinde@gmail.com>
2023-11-10 20:00:10 +0530

    fix: remove log

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -5,7 +5,6 @@ import { usePathname } from 'next/navigation';
 
 const NavBar = () => {
     const currentPath = usePathname();
-    console.log(currentPath)
 
     const links = [
         { label: 'Homepage', href: '/' },
