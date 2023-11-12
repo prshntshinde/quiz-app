7736b01bc9aaa805bd4784de05dc1548b9210266
prshntshinde <mr.pshinde@gmail.com>
2023-11-12 17:40:07 +0530

    fix: remove unused functions

--- app/components/QuizMain.js
+++ app/components/QuizMain.js
@@ -6,19 +6,20 @@ const noOfQuestions = 50;
 const QuizMain = props => {
   return (
     <div className='grid grid-cols-12 gap-2 text-center text-4xl'>
-        <div style={{backgroundColor: `${getRandomColor()}`}}>01</div>
-        <div style={{backgroundColor: `${getRandomColor()}`}}>02</div>
-        <div style={{backgroundColor: `${getRandomColor()}`}}>03</div>
-        <div style={{backgroundColor: `${getRandomColor()}`}}>04</div>
-        <div style={{backgroundColor: `${getRandomColor()}`}}>05</div>
-        <div style={{backgroundColor: `${getRandomColor()}`}}>06</div>
-        <div style={{backgroundColor: `${getRandomColor()}`}}>07</div>
-        <div style={{backgroundColor: `${getRandomColor()}`}}>08</div>
-        <div style={{backgroundColor: `${getRandomColor()}`}}>09</div>
-        <div style={{backgroundColor: `${getRandomColor()}`}}>10</div>
-        <div style={{backgroundColor: `${getRandomColor()}`}}>11</div>
-        <div style={{backgroundColor: `${getRandomColor()}`}}>12</div>
-        <div style={{backgroundColor: `${getRandomColor()}`}}>13</div>
+        <div className='text-black bg-slate-400 hover:bg-cyan-400'>01</div>
+        <div className='text-black bg-slate-400 hover:bg-cyan-400'>02</div>
+        <div className='text-black bg-slate-400 hover:bg-cyan-400'>03</div>
+        <div className='text-black bg-slate-400 hover:bg-cyan-400'>04</div>
+        <div className='text-black bg-slate-400 hover:bg-cyan-400'>05</div>
+        <div className='text-black bg-slate-400 hover:bg-cyan-400'>06</div>
+        <div className='text-black bg-slate-400 hover:bg-cyan-400'>07</div>
+        <div className='text-black bg-slate-400 hover:bg-cyan-400'>08</div>
+        <div className='text-black bg-slate-400 hover:bg-cyan-400'>09</div>
+        <div className='text-black bg-slate-400 hover:bg-cyan-400'>10</div>
+        <div className='text-black bg-slate-400 hover:bg-cyan-400'>11</div>
+        <div className='text-black bg-slate-400 hover:bg-cyan-400'>12</div>
+        <div className='text-black bg-slate-400 hover:bg-cyan-400'>13</div>
+        <div className='text-black bg-slate-400 hover:bg-cyan-400'>14</div>
     </div>
   )
 }
@@ -26,7 +27,3 @@ const QuizMain = props => {
 QuizMain.propTypes = {}
 
 export default QuizMain
-
-function getRandomColor(){
-    return "#" + ((1<<24)*Math.random() | 0).toString(16);
-}
