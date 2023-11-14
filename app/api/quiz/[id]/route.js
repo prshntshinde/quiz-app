0b514e805eafed2cc5efc93e60aa0fe742be800f
prshntshinde <mr.pshinde@gmail.com>
2023-11-14 14:32:01 +0530

    feat: add update quiz api

---
+++ app/api/quiz/[id]/route.js
@@ -0,0 +1,11 @@
+import connectMongoDB from "@/libs/mongodb";
+import Quiz from "@/models/quiz";
+import { NextResponse } from "next/server";
+
+export async function PUT(request, { params }) {
+    const {id} = params;
+    const {newTitle: title, newDescription: description, newisComplete: isComplete, newisActive: isActive} = await request.json();
+    await connectMongoDB();
+    await Quiz.findByIdAndUpdate(id, {title, description, isComplete, isActive});
+    return NextResponse.json({message: "Quiz Updated"}, {status: 200});
+}
