1b03d7dbd24183d5bae5a34ebfa7b4d86b630192
prshntshinde <mr.pshinde@gmail.com>
2023-11-14 13:03:53 +0530

    feat: add get Quiz api

--- app/api/quiz/route.js
+++ app/api/quiz/route.js
@@ -3,18 +3,15 @@ import Quiz from "@/models/quiz";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-    const {title, description, isComplete, isActive} = await request.json();
+    const { title, description, isComplete, isActive } = await request.json();
     await connectMongoDB();
-    await Quiz.create({title, description, isComplete, isActive});
-    return NextResponse.json({message: "Quiz Created"}, {status: 201});
+    await Quiz.create({ title, description, isComplete, isActive });
+    return NextResponse.json({ message: "Quiz Created" }, { status: 201 });
 
 }
 
 export async function GET() {
     await connectMongoDB();
-    const res = await fetch('https://jsonplaceholder.typicode.com/users', {
-    })
-    const data = await res.json()
-   
-    return Response.json({ data })
-  }
+    const quizzes = await Quiz.find();
+    return NextResponse.json({ quizzes })
+}
