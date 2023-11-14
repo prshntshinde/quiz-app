9867f77d545c79b4b2a8c43fb8c940486fecb63e
prshntshinde <mr.pshinde@gmail.com>
2023-11-14 15:36:01 +0530

    feat: add get single quiz api

--- app/api/quiz/[id]/route.js
+++ app/api/quiz/[id]/route.js
@@ -3,9 +3,17 @@ import Quiz from "@/models/quiz";
 import { NextResponse } from "next/server";
 
 export async function PUT(request, { params }) {
-    const {id} = params;
-    const {newTitle: title, newDescription: description, newisComplete: isComplete, newisActive: isActive} = await request.json();
+    const { id } = params;
+    const { newTitle: title, newDescription: description, newisComplete: isComplete, newisActive: isActive } = await request.json();
     await connectMongoDB();
-    await Quiz.findByIdAndUpdate(id, {title, description, isComplete, isActive});
-    return NextResponse.json({message: "Quiz Updated"}, {status: 200});
+    await Quiz.findByIdAndUpdate(id, { title, description, isComplete, isActive });
+    return NextResponse.json({ message: "Quiz Updated" }, { status: 200 });
+}
+
+export async function GET(request, { params }) {
+    const { id } = params;
+    await connectMongoDB();
+    const quiz = await Quiz.findOne({ _id: id });
+    return NextResponse.json({ quiz }, { status: 200 });
+
 }
