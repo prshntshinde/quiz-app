bf9cd93e027d60740aaa07f721744d7068899b21
prshntshinde <mr.pshinde@gmail.com>
2023-11-25 14:28:14 +0530

    fix: remove unused imports

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from './components/NavBar'
-import HomeImage from './components/HomeImage'
 
 const inter = Inter({ subsets: ['latin'] })
 
