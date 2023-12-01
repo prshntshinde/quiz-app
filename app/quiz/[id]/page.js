63c1e6634b0b582851c84c5fec1b364d3c3e5dae
prshntshinde <mr.pshinde@gmail.com>
2023-12-01 10:45:16 +0530

    fix: width of questions card - closes #2

--- app/quiz/[id]/page.js
+++ app/quiz/[id]/page.js
@@ -1,3 +1,4 @@
+import Question from "@/app/components/Question";
 import Question1 from "@/app/components/Question1"
 import { fetchQuestions } from "@/libs/data"
 
@@ -9,18 +10,18 @@ export default async function AnswerPage() {
   return (
     <main>
       <div className="grid grid-cols-8 gap-2">
-      {question.map((q) => (
-        <div key={q._id} className="pb-3">
-          <Question1 question_id={q.question_id}/>
-        </div>
-      ))}
+        {question.map((q) => (
+          <div key={q._id} className="pb-3">
+            <Question1 question_id={q.question_id} />
+          </div>
+        ))}
       </div>
     </main>
   )
 }
 
-export function generateMetadata({params}) {
-  return{
+export function generateMetadata({ params }) {
+  return {
     title: "Questions | Quiz App"
   }
 }
