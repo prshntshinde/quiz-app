33333be11683b815e5968ff087c79235f7a3d5b1
prshntshinde <mr.pshinde@gmail.com>
2023-12-02 10:31:28 +0530

    feat: add explanation to quiz modal - add numbers in Question. - closes #7

--- app/components/Question1.js
+++ app/components/Question1.js
@@ -15,7 +15,7 @@ export default function Question1(props) {
                     <div>
                         <div className="flex justify-center text-4xl font-semibold"><CountdownTimer /></div>
 
-                        <div className="flex justify-center mt-3 outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold text-2xl"><p>{props.question}</p></div>
+                        <div className="flex justify-center mt-3 outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold text-2xl"><p>{props.question_id}. {props.question}</p></div>
                         <div className="grid grid-cols-2 justify-center">
                             <div><button className="outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold">{props.option1}</button></div>
                             <div><button className="outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold">{props.option2}</button></div>
@@ -23,8 +23,11 @@ export default function Question1(props) {
                             <div><button className="outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold">{props.option4}</button></div>
 
                         </div>
-                        <div className="flex justify-center outline outline-offset-0 outline-1 hover:bg-blue-500 border-solid border-stone-50 py-2 px-4 font-semibold">
+                        <div className="flex justify-center outline outline-offset-0 outline-1 hover:bg-blue-500 border-solid border-stone-50 py-2 px-4 font-semibold mb-3">
                             <button className="">Submit</button>
+                        </div>
+                        <div className="flex justify-center outline outline-offset-0 outline-1 hover:bg-gray-500 border-solid border-stone-50 py-2 px-4 font-semibold">
+                            {props.explanation}
                         </div>
                     </div>
                 </Modal>
--- app/quiz/[id]/page.js
+++ app/quiz/[id]/page.js
@@ -12,7 +12,7 @@ export default async function AnswerPage() {
       <div className="grid grid-cols-8 gap-2">
         {question.map((q) => (
           <div key={q._id} className="pb-3">
-            <Question1 question_id={q.question_id} question={q.question} option1={q.options[0][0]} option2={q.options[0][1]} option3={q.options[0][2]} option4={q.options[0][3]} />
+            <Question1 question_id={q.question_id} question={q.question} option1={q.options[0][0]} option2={q.options[0][1]} option3={q.options[0][2]} option4={q.options[0][3]} explanation={q.explanation} />
           </div>
         ))}
       </div>
