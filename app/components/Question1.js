29330aef02abdc28aa8f0b082c9defb5a1ef373f
prshntshinde <mr.pshinde@gmail.com>
2023-12-02 10:36:20 +0530

    style: add style to explanation. - add dash border - change background color - closes #8

--- app/components/Question1.js
+++ app/components/Question1.js
@@ -26,7 +26,7 @@ export default function Question1(props) {
                         <div className="flex justify-center outline outline-offset-0 outline-1 hover:bg-blue-500 border-solid border-stone-50 py-2 px-4 font-semibold mb-3">
                             <button className="">Submit</button>
                         </div>
-                        <div className="flex justify-center outline outline-offset-0 outline-1 hover:bg-gray-500 border-solid border-stone-50 py-2 px-4 font-semibold">
+                        <div className="flex justify-center outline outline-offset-0 outline-3 outline-yellow-500 hover:bg-yellow-200 hover:outline-dashed border-solid border-stone-50 py-2 px-4 font-semibold">
                             {props.explanation}
                         </div>
                     </div>
