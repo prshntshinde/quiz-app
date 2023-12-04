67befb037a30362d3fc747196bdcbf0f5aa40e7a
prshntshinde <mr.pshinde@gmail.com>
2023-12-04 18:44:58 +0530

    feat: add icons for answer state closes #11

--- app/components/Question1.js
+++ app/components/Question1.js
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import Modal from "./Modal";
 import CountdownTimer from "./CountdownTimer";
+import { RxCheckCircled, RxCrossCircled } from "react-icons/rx";
 
 export default function Question1(props) {
     console.log(props);
@@ -102,7 +103,8 @@ export default function Question1(props) {
 
                         </div>
                         <div className="flex justify-center py-2 px-4 font-semibold mb-3 animate-bounce">
-                            <p className={answerStatus === "Correct" ? "text-green-600" : "text-red-600"}>{answerStatus}</p>
+                            <p className={answerStatus === "Correct" ? "text-green-600" : "text-red-600"}>{answerStatus} </p>
+                            <p className="pl-1">{answerStatus === "Correct" ? <RxCheckCircled size={25} className="text-green-600" /> : <RxCrossCircled size={25} className="text-red-600" />}</p>
                         </div>
                         <div className="flex justify-center outline outline-offset-0 outline-1 hover:bg-blue-500 border-solid border-stone-50 py-2 px-4 font-semibold mb-3">
                             <button onClick={checkAnswer} disabled={selectedAnswer === null}>Submit</button>
