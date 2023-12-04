150ee53224838f5e16a69dd51e4071a31c57c582
prshntshinde <mr.pshinde@gmail.com>
2023-12-04 11:40:41 +0530

    feat: add check answer logic. - display if the answer is correct or wrong. closes #9

--- app/components/Question1.js
+++ app/components/Question1.js
@@ -1,12 +1,74 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "./Modal";
 import CountdownTimer from "./CountdownTimer";
 
 export default function Question1(props) {
     console.log(props);
     const [showModal, setShowModal] = useState(false);
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [answerStatus, setAnswerStatus] = useState("");
+
+    // const onAnswerSelected = (answer, index) => {
+    //     setSelectedAnswerIndex(index)
+
+    // }
+
+    // useEffect(() => {
+    //     console.log("use effect called");
+    // }, [selectedAnswer])
+
+    // var answerStatus = "R";
+
+    const updateSelectedAnswer = (index) => {
+        setSelectedAnswer(index);
+        if (props.answer === index) {
+            console.log("correct answer");
+            console.log(index);
+            console.log("Selected Answer: ", selectedAnswer);
+
+            // element.target.classList.add("correct")
+        }
+        else {
+            // element.target.classList.add("wrong")
+            console.log("wrong answer");
+            console.log(index);
+            console.log("Selected Answer: ", selectedAnswer);
+
+        }
+
+    }
+
+    const checkAnswer = () => {
+        // console.log("event ", e, "answer ", ans)
+        // setOption(ans);
+        // console.log("option ", option)
+        if (props.answer === selectedAnswer) {
+            // e.target.classList.add("correct");
+            console.log("correct answer");
+            // answerStatus = "Correct";
+            setAnswerStatus("Correct");
+        }
+        else {
+            // e.target.classList.add("wrong");
+            console.log("wrong answer");
+            // answerStatus = "Wrong";
+            setAnswerStatus("Wrong");
+        }
+    }
+
+    // const ssetSelectedAnswer = (element, ans) => {
+    //     console.log("props ans ", props.answer);
+    //     console.log("ans ", ans);
+    //     if (props.answer === ans) {
+    //         console.log("con true");
+    //         element.target.classList.add("correct")
+    //     }
+    //     else {
+    //         element.target.classList.add("wrong")
+    //     }
+    // }
     return (
         <div>
             <button className="w-32 shadow-xl outline outline-offset-0 outline-1 hover:bg-blue-500 text-black font-semibold hover:text-white border-solid border-stone-50 py-2 px-4 hover:border-transparent text-6xl" onClick={() => setShowModal(true)}>{props.question_id}</button>
@@ -16,15 +78,34 @@ export default function Question1(props) {
                         <div className="flex justify-center text-4xl font-semibold"><CountdownTimer /></div>
 
                         <div className="flex justify-center mt-3 outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold text-2xl"><p>{props.question_id}. {props.question}</p></div>
-                        <div className="grid grid-cols-2 justify-center">
-                            <div><button className="outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold">{props.option1}</button></div>
-                            <div><button className="outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold">{props.option2}</button></div>
-                            <div><button className="outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold">{props.option3}</button></div>
-                            <div><button className="outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold">{props.option4}</button></div>
+                        <div className="grid grid-cols-2 justify-center gap-3">
+                            {/* <div><button className="outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold hover:bg-blue-500">{props.option1}</button></div>
+                            <div><button className="outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold hover:bg-blue-500">{props.option2}</button></div>
+                            <div><button className="outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold hover:bg-blue-500">{props.option3}</button></div>
+                            <div><button className="outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold hover:bg-blue-500">{props.option4}</button></div> */}
+
+                            {/* Other implementation for answer logic*/}
+                            <ul >
+                                <div><li onClick={() => { updateSelectedAnswer(0) }} className="outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold hover:bg-blue-300">{props.option1}</li></div>
+                            </ul>
+
+
+                            <ul >
+                                <div><li onClick={() => { updateSelectedAnswer(1) }} className="outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold hover:bg-blue-300">{props.option2}</li></div>
+                            </ul>
+                            <ul >
+                                <div><li onClick={() => { updateSelectedAnswer(2) }} className="outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold hover:bg-blue-300">{props.option3}</li></div>
+                            </ul>
+                            <ul >
+                                <div><li onClick={() => { updateSelectedAnswer(3) }} className="outline outline-offset-0 outline-1 border-solid border-stone-50 py-2 px-4 mb-3 font-semibold hover:bg-blue-300">{props.option4}</li></div>
+                            </ul>
 
                         </div>
+                        <div className="flex justify-center py-2 px-4 font-semibold mb-3">
+                            <p>{answerStatus}</p>
+                        </div>
                         <div className="flex justify-center outline outline-offset-0 outline-1 hover:bg-blue-500 border-solid border-stone-50 py-2 px-4 font-semibold mb-3">
-                            <button className="">Submit</button>
+                            <button onClick={checkAnswer}>Submit</button>
                         </div>
                         <div className="flex justify-center outline outline-offset-0 outline-3 outline-yellow-500 hover:bg-yellow-200 hover:outline-dashed border-solid border-stone-50 py-2 px-4 font-semibold">
                             {props.explanation}
