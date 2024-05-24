f73bfe3a9aabec2705f7f912460251a2ddc2ff00
prshntshinde <mr.pshinde@gmail.com>
2024-05-24 18:17:35 +0530

    fix: DOM elements with ARIA roles should have a valid non-abstract role

--- app/components/QuizSkeleton.js
+++ app/components/QuizSkeleton.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function QuizSkeleton() {
   return (
     <div>
-      <div role="output" className="space-y-2.5 animate-pulse max-w-lg">
+      <div role="generic" className="space-y-2.5 animate-pulse max-w-lg">
         <div className="flex items-center w-full">
           <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
           <div className="h-2.5 ms-2 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
