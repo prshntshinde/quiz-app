6e70bdcc9eab44b48a5aeca0f526d1c9109f52ef
prshntshinde <mr.pshinde@gmail.com>
2024-05-24 15:59:53 +0530

    feat: Question modal entire screen. - Add cache actions

--- app/components/Modal.js
+++ app/components/Modal.js
@@ -8,7 +8,7 @@ export default function Modal(props) {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-25 backdrop-blur-sm">
-      <div className="w-[1050px] flex flex-col">
+      <div className="flex flex-col w-screen">
         <button
           className="text-xl font-bold text-red-500 place-self-end"
           onClick={() => props.onClose()}
