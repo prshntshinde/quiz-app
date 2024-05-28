161c69696d29c48294cee9cd92cc29cbbc3f20dd
prshntshinde <mr.pshinde@gmail.com>
2024-05-28 16:25:42 +0530

    feat: format the contenct section

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -3,8 +3,15 @@ import SideMenu from "../components/SideMenu";
 export default function AdminLayout({ children }) {
   return (
     <>
-      <SideMenu />
-      {children}
+      <div className="flex h-screen overflow-hidden">
+        <SideMenu />
+        <section id="content"></section>
+        <div className="relative flex flex-col overflow-x-hidden overflow-y-auto">
+          <div className="p-4 mx-auto max-w-screen-2xl md:p-6 2xl:p-10">
+            {children}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- app/components/SideMenu.js
+++ app/components/SideMenu.js
@@ -5,17 +5,19 @@ export default function SideMenu() {
     { label: "Questions", href: "/admin/questions" },
   ];
   return (
-    <div className="flex flex-col justify-between h-screen text-center bg-white border-e">
-      <ul className="px-4 mt-3 space-y-1">
-        {links.map((link) => (
-          <li
-            key={link.href}
-            className="block px-4 py-2 font-bold border rounded-lg text-black-600 text-md hover:bg-sky-100 hover:text-blue-600 border-slate-300"
-          >
-            <a href={link.href}>{link.label}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <section id="sidebar">
+      <aside className=" relative flex flex-col gap-1 justify-between text-center bg-white border-e min-w-[240px] h-[calc(100vh-2rem)] w-full max-w-[20rem]">
+        <ul className="px-4 mt-3 space-y-1">
+          {links.map((link) => (
+            <li
+              key={link.href}
+              className="block px-4 py-2 font-bold border rounded-lg text-black-600 text-md hover:bg-sky-100 hover:text-blue-600 border-slate-300"
+            >
+              <a href={link.href}>{link.label}</a>
+            </li>
+          ))}
+        </ul>
+      </aside>
+    </section>
   );
 }
