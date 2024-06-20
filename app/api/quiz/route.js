590776a95b8e46ebb6a6a4d67989db2c660cbfb2
prshntshinde <mr.pshinde@gmail.com>
2024-06-20 11:19:53 +0530

    feat: add versioned API to create quiz (#40) - add connection state of MongoDB - Check already connected status - Check connecting status

--- app/api/quiz/route.js
+++ app/api/quiz/route.js
@@ -1,31 +1,28 @@
-import connectMongoDB from "@/libs/mongodb"
-import {Quiz} from "@/models/quiz";
+import connectMongoDB from "@/libs/mongodb";
+import { Quiz } from "@/models/quiz";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-    const { title, description, isComplete, isActive } = await request.json();
-    await connectMongoDB();
-    await Quiz.create({ title, description, isComplete, isActive });
-    return NextResponse.json({ message: "Quiz Created" }, { status: 201 });
-
+  const { title, description, isComplete, isActive } = await request.json();
+  await connectMongoDB();
+  await Quiz.create({ title, description, isComplete, isActive });
+  return NextResponse.json({ message: "Quiz Created" }, { status: 201 });
 }
 
 export async function GET() {
-    try {
-        await connectMongoDB();
-        const quizzes = await Quiz.find();
-        return NextResponse.json({ quizzes })
-
-    } catch (error) {
-        console.log(error);
-        throw new Error("Failed to fetch Quizzes")
-    }
-
+  try {
+    await connectMongoDB();
+    const quizzes = await Quiz.find();
+    return NextResponse.json({ quizzes });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to fetch Quizzes");
+  }
 }
 
 export async function DELETE(request) {
-    const id = request.nextUrl.searchParams.get("id");
-    await connectMongoDB();
-    await Quiz.findByIdAndDelete(id);
-    return NextResponse.json({ message: "Quiz Deleted." }, { status: 200 });
+  const id = request.nextUrl.searchParams.get("id");
+  await connectMongoDB();
+  await Quiz.findByIdAndDelete(id);
+  return NextResponse.json({ message: "Quiz Deleted." }, { status: 200 });
 }
